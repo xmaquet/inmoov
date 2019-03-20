6c9b0d42ee8af545ea2c2b9c028af2bca44a325f
xmaquet <34754589+xmaquet@users.noreply.github.com>
2019-03-20 23:01:21 +0100

    deserialize fonctionne - à vérifier

--- arduino/8relayControl/8relayControl.ino
+++ arduino/8relayControl/8relayControl.ino
@@ -11,12 +11,15 @@ String controlerTitle = "Contrôleur de commande 8 relais" ;
 String sketchName = "8relayControl" ; 
 int relay[8] = {2,3,4,5,6,7,8,9}; //affectation d'un relay à chaque broche digitale
 boolean relayState[8] = {1,1,1,1,1,1,1,1}; //état initial de chaque relais
-String returnMsg [5] ={ //messages à renvoyer selon les cas 
+String returnMsg [8] ={ //messages à renvoyer selon les cas 
   "Order received in a correct format", //l'ordre reçu est correctement formaté
   "Bad pin number in order", // la broche évoquée n'est pas correcte
   "Controler not accept action order", // les ordres d'action ne sont pas acceptés par ce controleur
   "Controler not accept query order", // les ordres de requête ne sont pas acceptés par ce controleur
   "Value is not correct" // la valeur indiquée est incorrecte
+  "Controler not manage servo" // le contrôleur ne contrôle pas de servo
+  "Controler not manage sensor" //le controleur ne controle pas de senseur
+  "Controler not manage ledRing" // le controleur ne controle pas d'anneau de leds
 };
 String incoming = "";
 int start = 0;
@@ -55,37 +58,54 @@ void loop() {
 void parseIncoming(String text){//interprétation de la chaîne reçue entre [] = deserialize JSON
   text.replace("[","");//retirer les [
   text.replace("]","");//retirer les ]
-  char escapeCar = '"';
-  String car = "" ;
-  car += "\\" ;
-  car += escapeCar ;
-  text.replace("\"",car);//échapper les guillements
-  const char* text2char = string2char(text);
   Serial.println (text);
-
-  DynamicJsonDocument doc(1024);
-  //StaticJsonDocument<2048> doc;
-  Serial.println(doc.capacity());
-  //deserializeJson(doc, text2char);
-  DeserializationError err = deserializeJson(doc, text);
-  if (err) {
-    Serial.print(F("deserializeJson() failed with code "));
-    Serial.println(err.c_str());
-    }
-  JsonObject obj = doc.as<JsonObject>();
-  for (JsonPair p : obj) {
-    p.key(); // is a JsonString
-    p.value(); // is a JsonVariant
-    }
-  if (obj.containsKey("params")) {Serial.println("order OK");}
+      DynamicJsonDocument doc(512);
+      DeserializationError err = deserializeJson(doc, text);
+      if (err) {
+              Serial.print(F("deserializeJson() failed with code "));
+              Serial.println(err.c_str());
+              }
+      JsonObject obj = doc.as<JsonObject>();
+      // Loop through all the key-value pairs in obj
+        for (JsonPair p : obj) {
+        p.key() ;// is a JsonString
+        p.value() ;// is a JsonVariant
+        }
+        
+      //obj est l'objet qui contient la représentation du json reçu
+      if (obj.containsKey("order")&& obj.containsKey("params")){
+        f_returnMsg(0);
+        if (obj.containsKey("servo")){
+          f_returnMsg(5);
+        }
+        if (obj.containsKey("sensor")){
+          f_returnMsg(6);
+        }
+        if (obj.containsKey("ledRing")){
+          f_returnMsg(7);
+        }
+        if (obj.containsKey("relay")){
+          int value = obj["params"]["value"];
+          int index = obj["params"]["index"];
+          int closeLevel = obj["relay"]["closeLevel"];
+          f_actionRelay (value, index, closeLevel);
+        }
+      }
+          
 }
 
-void actionRelay(int relayIndex, String level){//action sur le relais indexé
+void f_actionRelay(int val, int relayIndex, int level){//action sur le relais indexé
+  switch (val) {
+    case 0:
+      digitalWrite(relay[relayIndex],!level);
+      break;
+    case 1:
+    digitalWrite(relay[relayIndex],level);
+      break;    
+  }
   
 }
-char* string2char(String command){
-    if(command.length()!=0){
-        char *p = const_cast<char*>(command.c_str());
-        return p;
-    }
+
+void f_returnMsg (int indexMsg) {
+  Serial.println (returnMsg [indexMsg]);
 }
